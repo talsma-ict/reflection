60635034974766f290687eca0652845c73ebecef
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2018-04-16 08:41:21 +0200

    [#25] Update check for release tag to semver 2.0 compatible regex

--- .travis/release.sh
+++ .travis/release.sh
@@ -18,8 +18,8 @@ if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
     echo "Not releasing from pull-request ${TRAVIS_PULL_REQUEST}."
 elif [ ! "${TRAVIS_BRANCH}" =~ ^develop|master$ ]; then
     echo "Not releasing from branch ${TRAVIS_BRANCH}."
-elif [[ ! "${TRAVIS_TAG}" =~ ^release-[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+.*$ ]]; then
-    echo "No 'release-x.y.z' tag found, skipping release.' "
+elif [[ ! "${TRAVIS_TAG}" =~ ^release\-[0-9]+\.[0-9]+\.[0-9]+(-[A-Za-z0-9]+)*(\+[A-Za-z0-9\.\-]+)?$ ]]; then
+    echo "No 'release-x.y.z' tag found, skipping release."
 else
     echo "Performing ${TRAVIS_TAG} from branch ${TRAVIS_BRANCH}."
 fi
