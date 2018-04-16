0154911965f21e48b5b324ed577155e4a5329c26
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2018-04-16 09:58:48 +0200

    [#25] Separate release prefix check from semantic version check

--- .travis/release.sh
+++ .travis/release.sh
@@ -3,6 +3,16 @@
 set -euo pipefail
 set -x
 
+is_semver_version() {
+    # Test whether the first argument matches a semantic version, see https://semver.org/
+    if [[ ${1:-} =~ ^[0-9]+\.[0-9]+\.[0-9]+(-[A-Za-z0-9\.]+)*(\+[A-Za-z0-9\.\-]+)?$ ]]; then return 0; else return 1; fi
+}
+
+is_release_version() {
+    # Test whether the first argument starts with either 'release-' or 'release/' followed by a valid semantic version
+    if [[ ${1:-} =~ ^release[/\-].*$ ]]; then is_semver_version `echo ${1} | sed 's/^release[/-]//'`; else return 1; fi
+}
+
 #----------------------
 # MAIN
 #----------------------
@@ -16,9 +26,11 @@ set -x
 
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
     echo "Not releasing from pull-request ${TRAVIS_PULL_REQUEST}."
-elif [ ! "${TRAVIS_BRANCH}" =~ ^develop|master$ ]; then
-    echo "Not releasing from branch ${TRAVIS_BRANCH}."
-elif [[ ! "${TRAVIS_TAG}" =~ ^release\-[0-9]+\.[0-9]+\.[0-9]+(-[A-Za-z0-9]+)*(\+[A-Za-z0-9\.\-]+)?$ ]]; then
+elif is_release_version ${TRAVIS_BRANCH}; then
+    echo "Releasing from branch ${TRAVIS_BRANCH}."
+elif [[ ! "${TRAVIS_BRANCH}" =~ ^develop|master$ ]]; then
+    echo "Not releasing from branch '${TRAVIS_BRANCH}'."
+elif ! is_release_version ${TRAVIS_TAG}; then
     echo "No 'release-x.y.z' tag found, skipping release."
 else
     echo "Performing ${TRAVIS_TAG} from branch ${TRAVIS_BRANCH}."
