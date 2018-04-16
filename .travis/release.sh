0cd78abbe7bf10ee169441e17b71f919b089c63a
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2018-04-16 10:57:05 +0200

    [#25] Respond to 'release-x.y.z' tag by creating a release/x.y.z branch

--- .travis/release.sh
+++ .travis/release.sh
@@ -3,14 +3,26 @@
 set -euo pipefail
 set -x
 
-is_semver_version() {
+is_semantic_version() {
     # Test whether the first argument matches a semantic version, see https://semver.org/
     if [[ ${1:-} =~ ^[0-9]+\.[0-9]+\.[0-9]+(-[A-Za-z0-9\.]+)*(\+[A-Za-z0-9\.\-]+)?$ ]]; then return 0; else return 1; fi
 }
 
 is_release_version() {
     # Test whether the first argument starts with either 'release-' or 'release/' followed by a valid semantic version
-    if [[ ${1:-} =~ ^release[/\-].*$ ]]; then is_semver_version `echo ${1} | sed 's/^release[/-]//'`; else return 1; fi
+    if [[ ${1:-} =~ ^release[/\-].*$ ]]; then is_semantic_version `echo ${1} | sed 's/^release[/-]//'`; else return 1; fi
+}
+
+create_release_branch() {
+    release_version="${1:-}"
+    if ! is_semantic_version ${release_version}; then
+        echo "Not a valid version to be released: '${release_version}'!"
+        exit 1
+    fi
+    echo "Removing tag 'release-${release_version}' from ${TRAVIS_BRANCH} and pushing new 'release/${release_version}' branch..."
+    git push --delete origin release-${release_version}
+    git checkout -b release/${release_version}
+    git push origin release/${release_version}
 }
 
 #----------------------
@@ -33,5 +45,5 @@ elif [[ ! "${TRAVIS_BRANCH}" =~ ^develop|master$ ]]; then
 elif ! is_release_version ${TRAVIS_TAG}; then
     echo "No 'release-x.y.z' tag found, skipping release."
 else
-    echo "Performing ${TRAVIS_TAG} from branch ${TRAVIS_BRANCH}."
+    create_release_branch `echo ${TRAVIS_TAG} | sed 's/^release-//'`
 fi
