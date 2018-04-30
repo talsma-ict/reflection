ca0ea79cdb5fcfff9715a27156b439243e3efa86
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2018-04-30 09:31:52 +0200

    [#25] More fetch info

--- .travis/git-functions.sh
+++ .travis/git-functions.sh
@@ -34,7 +34,7 @@ find_remote_branch() {
 
 switch_to_branch() {
     log "Switching to branch ${1}"
-    git fetch
+    git fetch -v --unshallow -p -u origin
     git checkout "${1}" || git checkout -b "${1}" "origin/${1}"
     git pull
 }
